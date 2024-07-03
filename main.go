7dd7b3f5549aeb7f78e00a9d70076463dd1819c3
ricardovalenca <ricardo_valenca@hotmail.com>
2024-07-03 13:23:04 -0300

    Add more comments to package TestGodoc

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
-// Package created just for testing purposes.
+/*
+Test package for godoc.org.
+Just my first project uploading project do godoc for documenting.
+
+	%1	Test
+	%2	Test2
+	%3	Test3
+*/
 package TestGodoc
 
 import "fmt"
