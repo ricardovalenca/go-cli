9d41392cd4aef380f9a7d665278a63809ccc90fb
ricardovalenca <ricardo_valenca@hotmail.com>
2024-07-03 10:54:03 -0300

    Creating test for godoc.org

---
+++ main.go
@@ -0,0 +1,9 @@
+// Package created just for testing purposes.
+package TestGodoc
+
+import "fmt"
+
+// Function created only for test purposes
+func Test() {
+	fmt.Println("Just testing godoc.org")
+}
